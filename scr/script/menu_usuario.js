2e80f07f44b71a665575ef33afff835afe442d13
Xua1zin <jtoscanalbieri@gmail.com>
2023-10-11 01:24:12 -0300

    criação carrossel

--- scr/script/menu_usuario.js
+++ scr/script/menu_usuario.js
@@ -6,13 +6,45 @@ function abrirMenu() {
         ligar.style.display = "none";
     }
 }
-let user = {
-    nome: "Rafaelzinho123"
-};
+let user = {nome: "Rafaelzinho123"};
 function menuUsuario(user){
     let username = document.getElementById("nomeUsuario");
     username.innerText = user.nome;
     console.log(user.nome);
 }
+
+const controls = document.querySelectorAll('.control');
+let currentItem = 0;
+const item = document.querySelectorAll('.item');
+const maxItem = item.length;
+
+controls.forEach(control => {
+    control.addEventListener('click', () => {
+        const isLeft = control.classList.contains('arrowLeft');
+    
+        if(isLeft){
+            currentItem -= 1;
+        }
+        else{
+            currentItem += 1;
+        }
+
+        if(currentItem >= maxItem){
+            currentItem = 0;
+        }
+        if(currentItem < 0){
+            currentItem = maxItem - 1;
+        }
+
+        console.log("control", isLeft, currentItem);
+        item.forEach(item => item.classList.remove('currentItem'));
+
+        item[currentItem].scrollIntoView({
+            inline:"center",
+            behavior: "smooth"
+        })
+        item[currentItem]. classList.add("currentItem");
+    })
+})
 
 menuUsuario(user);
