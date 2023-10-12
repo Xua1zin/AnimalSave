2b97b0e89b1c1f0130e20acdbb527de82b1c17c5
Xua1zin <jtoscanalbieri@gmail.com>
2023-10-12 16:07:40 -0300

    Finalização carrossel

--- scr/script/menu_usuario.js
+++ scr/script/menu_usuario.js
@@ -6,14 +6,14 @@ function abrirMenu() {
         ligar.style.display = "none";
     }
 }
-let user = {nome: "Rafaelzinho123"};
-function menuUsuario(user){
+let user = { nome: "Rafaelzinho123" };
+function menuUsuario(user) {
     let username = document.getElementById("nomeUsuario");
     username.innerText = user.nome;
     console.log(user.nome);
 }
-
-const controls = document.querySelectorAll('.control');
+menuUsuario(user);
+/*const controls = document.querySelectorAll('.control');
 let currentItem = 0;
 const item = document.querySelectorAll('.item');
 const maxItem = item.length;
@@ -45,6 +45,58 @@ controls.forEach(control => {
         })
         item[currentItem]. classList.add("currentItem");
     })
-})
+})*/
+function moveToSelected(element) {
+    if (element == "next") {
+        var selected = $(".selected").next();
+    } else if (element == "prev") {
+        var selected = $(".selected").prev();
+    } else {
+        var selected = element;
+    }
 
-menuUsuario(user);
+    var next = $(selected).next();
+    var prev = $(selected).prev();
+    var prevSecond = $(prev).prev();
+    var nextSecond = $(next).next();
+
+    $(selected).removeClass().addClass("selected");
+
+    $(prev).removeClass().addClass("prev");
+    $(next).removeClass().addClass("next");
+
+    $(nextSecond).removeClass().addClass("nextRightSecond");
+    $(prevSecond).removeClass().addClass("prevLeftSecond");
+
+    $(nextSecond).nextAll().removeClass().addClass("hideRight");
+    $(prevSecond).prevAll().removeClass().addClass("hideLeft");
+}
+
+// Eventos teclado
+$(document).keydown(function (e) {
+    switch (e.which) {
+        case 37: // left
+            moveToSelected("prev");
+            break;
+
+        case 39: // right
+            moveToSelected("next");
+            break;
+
+        default:
+            return;
+    }
+    e.preventDefault();
+});
+
+$("#carousel div").click(function () {
+    moveToSelected($(this));
+});
+
+$("#prev").click(function () {
+    moveToSelected("prev");
+});
+
+$("#next").click(function () {
+    moveToSelected("next");
+});
